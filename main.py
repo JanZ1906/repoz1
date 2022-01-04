0b3f8d9d7a5cd044759cee703dc5256ac21b1c48
JanZ1906 <jan.w.zastawa@gmail.com>
2022-01-04 14:49:55 +0100

    Update main.py
    nwd

--- main.py
+++ main.py
@@ -1,26 +1,12 @@
-text_1 = input("Podaj tekst 1: ")
-text_2 = input("Podaj tekst 2: ")
+def nwd (a, b):
 
-is_valid = True
+    while b:
+        temp = a
+        a = b
+        b = temp%b
+    return a
 
-if len(text_1) == len(text_2):
+a = int(input('Podaj pierwsza liczbe: '))
+b = int(input('Podaj druga liczbe: '))
 
-    tmp_dict = {}
-
-    for i in text_1:
-        if i in tmp_dict:
-            tmp_dict[i] = tmp_dict[i] + 1
-        else:
-            tmp_dict[i] = 1
-
-    for j in text_2:
-        if (j not in tmp_dict) or (tmp_dict[j] <= 0):
-            is_valid = False
-            break
-else:
-    is_valid = False
-
-if is_valid:
-    print("Teksty są anagramami.")
-else:
-    print("Teksty nie są anagramami.")
+print("NWD(a, b) = " + str(nwd(a, b)))
